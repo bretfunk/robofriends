983b3833b63924d3db82415a25d61d7244627a22
BFunk <bret.e.funk@gmail.com>
2018-10-11 12:22:10 -0500

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map(({ id, name, email }, i) => (
+        <Card key={id} id={id} name={name} email={email} />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
